16a6a4be6010d22873e07a7fd1f6c5513604952c
khrunytsky <khrunytsky@gmail.com>
2025-12-18 18:29:56 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from etl import load_data, analyze_data
 from visuals import create_dashboard
 
 if __name__ == "__main__":
-    FILENAME = 'sprzedaz_rowery_2024V2.csv'
+    FILENAME = 'sprzedaz_rowery_2024.csv'
     
     try:
         raw_df = load_data(FILENAME)
